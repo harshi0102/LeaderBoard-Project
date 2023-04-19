3bfad51ebf52530df24fd0706730e614f53680cb
Harshika Govind <harshika0102@gmail.com>
2023-04-19 22:14:36 +0530

    fixed codes

--- src/result.js
+++ src/result.js
@@ -1,11 +1,11 @@
 class Result {
   static record(user, score) {
     return `
-      <div class="result" data-id=${user}>
-        <span class="user" data-id=${user}>${user}: </span>
-        <span class="score" data-id=${user}>${score}</span>
-      </div>
-      `;
+    <div class="result" data-id=${user}>
+      <span class="user" data-id=${user}>${user}: </span>
+      <span class="score" data-id=${user}>${score}</span>
+    </div>
+    `;
   }
 }
 
